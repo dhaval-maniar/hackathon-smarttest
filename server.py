33567702a8e98e8db6f3cb041988ff3739a3ada5
chiragjajuNX <chirag.jaju@nutanix.com>
2024-06-04 14:36:23 +0530

    add server .py

--- server.py
+++ server.py
@@ -1,30 +1,32 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from test import caller
- 
-app = App(token=os.getenv("SLACK_BOT_TOKEN"))
- 
-# Add functionality here later
-# @app.event("app_home_opened") etc.
- 
-@app.message(".*gerrit.eng.nutanix.com*")
-def message_hello(message, say):
-    # print(message)
-    # say(f"Hey there <@{message['user']}>!\nI heard you say {message['text']}")
-    # result = run_the_script()
-    # oid = "665cac7c2bc0c413c8fae9b2"
-    result = caller()
-    # time.sleep(1)
-    say(next(result))
-    # time.sleep(1)
-    say(next(result))
-    result_json = next(result)
-    message = f"Failed: {result_json['failed']}\nPassed: {result_json['passed']}\n"
-    for x in result_json['failed_test_cases']:
-        message += f"Failed Test Case: {x['name']}\nLog: {x['url']}\n------------------\n"
-    say(message)
- 
-# Ready? Start your app!
+from script import run_the_script
+
+# Initialize the Slack App with the bot token
+slack_app = App(token=os.getenv("SLACK_BOT_TOKEN"))
+
+# Define a message handler for messages containing "gerrit.eng.nutanix.com"
+@slack_app.message(".*gerrit.eng.nutanix.com*")
+def handle_message(message, say):
+    # Run the script and get the result
+    script_result = run_the_script()
+
+    # Send the first two status_updates as messages
+    say(next(script_result))
+    say(next(script_result))
+
+    # Get the JSON result from the script
+    result_json = next(script_result)
+
+    # Format a message with the failed and passed test cases
+    message_text = f"Failed: {result_json['failed']}\nPassed: {result_json['passed']}\n"
+    for test_case in result_json['failed_test_cases']:
+        message_text += f"Failed Test Case: {test_case['name']}\nLog: {test_case['url']}\n------------------\n"
+
+    # Send the formatted message with the urls
+    say(message_text)
+
+# Start the Slack App in Socket Mode
 if __name__ == "__main__":
-    SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
+    SocketModeHandler(slack_app, os.getenv("SLACK_APP_TOKEN")).start()
